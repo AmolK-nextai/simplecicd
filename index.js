45e0d04291e554d8d8642df0306cc97e39d88139
Amol Kharat <amol.k@nexaitech.com>
2024-03-07 17:29:52 +0530

    changes in code for testing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World")
+const express = require("express");
+
+
+
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on port ${PORT}`);
+  });
